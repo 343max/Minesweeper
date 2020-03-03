ffaacc17ddf1813299067c349e48b37cd7380855
Max von Webel <max.von-webel@grandcentrix.net>
2020-03-03 11:21:58 +0100

    getting closer

--- __tests__/Field.ts
+++ __tests__/Field.ts
@@ -1,14 +1,19 @@
-import { generateBombField } from "../src/model/Field"
+import {
+  generateBombField,
+  getAdjacentPatches,
+  getAdjacentBombCount,
+  getFieldSize
+} from "../src/model/Field"
 
 test("should generate a bomb field", () => {
-  const field = generateBombField(5, 6, 29, { x: 3, y: 4 })
+  const field = generateBombField({ width: 2, height: 3 }, 5, { x: 1, y: 2 })
 
   console.log(field)
 
-  expect(field.length).toBe(5)
+  expect(field.length).toBe(2)
 
   field.forEach(row => {
-    expect(row.length).toBe(6)
+    expect(row.length).toBe(3)
   })
 
   const bombCount = field.reduce((count, row) => {
@@ -16,6 +21,62 @@ test("should generate a bomb field", () => {
       return count + (patch ? 1 : 0)
     }, count)
   }, 0)
-  expect(bombCount).toBe(29)
-  expect(field[3][4]).toBe(false)
+  expect(bombCount).toBe(5)
+
+  expect(field[1][2]).toBe(false)
+})
+
+test("should have no adjacent patches", () => {
+  const patches = getAdjacentPatches({ width: 1, height: 1 }, { x: 0, y: 0 })
+  expect(patches).toEqual([])
+})
+
+test("should messure a generated field size correctly", () => {
+  const field = generateBombField({ width: 5, height: 3 }, 0, { x: 0, y: 0 })
+  const size = getFieldSize(field)
+  expect(size).toEqual({ width: 5, height: 3 })
+})
+
+test("should correctly messure an empty field", () => {
+  const size = getFieldSize([])
+  expect(size).toEqual({ width: 0, height: 0 })
+})
+
+test("should have 8 adjacent patches", () => {
+  const patches = getAdjacentPatches({ width: 3, height: 3 }, { x: 1, y: 1 })
+  expect(patches).toEqual([
+    { x: 0, y: 0 },
+    { x: 1, y: 0 },
+    { x: 2, y: 0 },
+    { x: 1, y: 0 },
+    // center patch
+    { x: 1, y: 2 },
+    { x: 2, y: 0 },
+    { x: 2, y: 1 },
+    { x: 2, y: 2 }
+  ])
+})
+
+test("should check nearby for empty", () => {
+  const count = getAdjacentBombCount(
+    [
+      [false, false, false],
+      [false, false, false],
+      [false, false, false]
+    ],
+    { x: 1, y: 1 }
+  )
+  expect(count).toBe(0)
+})
+
+test("should check nearby for bombs", () => {
+  const count = getAdjacentBombCount(
+    [
+      [true, true, true],
+      [true, false, true],
+      [true, true, true]
+    ],
+    { x: 1, y: 1 }
+  )
+  expect(count).toBe(8)
 })
--- src/model/Field.ts
+++ src/model/Field.ts
@@ -3,6 +3,11 @@ export interface PatchCoordinate {
   y: number
 }
 
+export interface FieldSize {
+  width: number
+  height: number
+}
+
 export enum VisiblePatch {
   Hidden,
   Empty,
@@ -18,12 +23,51 @@ export enum VisiblePatch {
   Nearby8
 }
 
+export type BoolField = boolean[][]
+
+interface FieldState {
+  bombs: BoolField
+  flags: BoolField
+  revealed: BoolField
+}
+
+export function getAdjacentPatches(
+  { width, height }: FieldSize,
+  { x, y }: PatchCoordinate
+): PatchCoordinate[] {
+  return [
+    { x: x - 1, y: y - 1 },
+    { x: x + 0, y: y - 1 },
+    { x: x + 1, y: y - 1 },
+    { x: x + 0, y: y - 1 },
+    // self patch
+    { x: x + 0, y: y + 1 },
+    { x: x + 1, y: y - 1 },
+    { x: x + 1, y: y + 0 },
+    { x: x + 1, y: y + 1 }
+  ].filter(({ x, y }) => {
+    return x >= 0 && y >= 0 && x < width && y < height
+  })
+}
+
+export function getFieldSize(field: BoolField): FieldSize {
+  return { width: field.length, height: field[0]?.length ?? 0 }
+}
+
+export function getAdjacentBombCount(
+  field: BoolField,
+  center: PatchCoordinate
+): number {
+  return getAdjacentPatches(getFieldSize(field), center).filter(({ x, y }) => {
+    return field[x][y]
+  }).length
+}
+
 export function generateBombField(
-  width: number,
-  height: number,
+  { width, height }: FieldSize,
   bombCount: number,
   safePatch: PatchCoordinate
-): boolean[][] {
+): BoolField {
   let field = Array(width)
     .fill(false, 0, width)
     .map(() => {
