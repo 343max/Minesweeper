7dfa19350e9fc903bf8036c63ea9548e930ed65d
Max von Webel <max.von-webel@grandcentrix.net>
2020-03-03 20:43:37 +0100

    it's working

--- src/model/Reveal.ts
+++ src/model/Reveal.ts
@@ -1,11 +1,47 @@
-import { FieldState, PatchCoordinate } from "./Field"
+import {
+  FieldState,
+  PatchCoordinate,
+  getFieldSize,
+  getAdjacentBombCount
+} from "./Field"
 
 export function revealPatch(
   field: FieldState,
   { x, y }: PatchCoordinate
 ): FieldState {
-  const patch = field[x][y]
-  patch.isRevealed = true
-  field[x][y] = patch
+  let toCheck = [{ x, y }]
+
+  const { width, height } = getFieldSize(field)
+
+  const validCoordinates = ({ x, y }: PatchCoordinate): boolean => {
+    return x >= 0 && y >= 0 && x < width && y < height
+  }
+
+  while (toCheck.length > 0) {
+    const { x, y } = toCheck.pop()
+
+    if (field[x][y].isRevealed || field[x][y].isBomb) {
+      continue
+    }
+
+    field[x][y].isRevealed = true
+
+    if (getAdjacentBombCount(field, { x, y }) == 0) {
+      toCheck = [
+        ...toCheck,
+        ...[
+          { x: x - 1, y },
+          { x, y: y - 1 },
+          { x: x + 1, y },
+          { x, y: y + 1 }
+        ].filter(({ x, y }) => {
+          return validCoordinates({ x, y }) && !field[x][y].isRevealed
+        })
+      ]
+    }
+  }
+
+  field[x][y].isRevealed = true
+
   return field
 }
