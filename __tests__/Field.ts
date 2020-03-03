b76204e594f8573c7e7d1437bd7484fdd260f67c
Max von Webel <max.von-webel@grandcentrix.net>
2020-03-03 13:15:11 +0100

    Game Field should be feature complete

--- __tests__/Field.ts
+++ __tests__/Field.ts
@@ -1,14 +1,30 @@
 import {
+  emptyField,
+  FieldState,
   generateBombField,
-  getAdjacentPatches,
   getAdjacentBombCount,
-  getFieldSize
+  getAdjacentPatches,
+  getFieldSize,
+  getVisibleField,
+  getVisiblePatch,
+  getVisiblePatchForBombCount,
+  VisibleField,
+  VisiblePatch,
+  BoolField
 } from "../src/model/Field"
+
+test("should generate an empty filed", () => {
+  const field = emptyField({ width: 2, height: 3 })
+  const expectedField: BoolField = [
+    [false, false, false],
+    [false, false, false]
+  ]
+
+  expect(field).toStrictEqual(expectedField)
+})
 
 test("should generate a bomb field", () => {
   const field = generateBombField({ width: 2, height: 3 }, 5, { x: 1, y: 2 })
-
-  console.log(field)
 
   expect(field.length).toBe(2)
 
@@ -79,4 +95,124 @@ test("should check nearby for bombs", () => {
     { x: 1, y: 1 }
   )
   expect(count).toBe(8)
+})
+
+test("should get Empty Patch", () => {
+  const patch = getVisiblePatchForBombCount(0)
+  expect(patch).toBe(VisiblePatch.Empty)
+})
+
+test("should get AdjacentBomb1", () => {
+  const patch = getVisiblePatchForBombCount(1)
+  expect(patch).toBe(VisiblePatch.AdjacentBomb1)
+})
+
+test("should get AdjacentBomb7", () => {
+  const patch = getVisiblePatchForBombCount(7)
+  expect(patch).toBe(VisiblePatch.AdjacentBomb7)
+})
+
+test("should show grass", () => {
+  const field: FieldState = {
+    bombs: [[false]],
+    flags: [[false]],
+    revealed: [[false]]
+  }
+  const patch = getVisiblePatch(field, { x: 0, y: 0 })
+  expect(patch).toBe(VisiblePatch.Grass)
+})
+
+test("should show grass for unrevealed bomb", () => {
+  const field: FieldState = {
+    bombs: [[true]],
+    flags: [[false]],
+    revealed: [[false]]
+  }
+  const patch = getVisiblePatch(field, { x: 0, y: 0 })
+  expect(patch).toBe(VisiblePatch.Grass)
+})
+
+test("should show empty", () => {
+  const field: FieldState = {
+    bombs: [[false]],
+    flags: [[false]],
+    revealed: [[true]]
+  }
+  const patch = getVisiblePatch(field, { x: 0, y: 0 })
+  expect(patch).toBe(VisiblePatch.Empty)
+})
+
+test("should show flag", () => {
+  const field: FieldState = {
+    bombs: [[false]],
+    flags: [[true]],
+    revealed: [[false]]
+  }
+  const patch = getVisiblePatch(field, { x: 0, y: 0 })
+  expect(patch).toBe(VisiblePatch.Flag)
+})
+
+test("should show flag for marekd bomb", () => {
+  const field: FieldState = {
+    bombs: [[true]],
+    flags: [[true]],
+    revealed: [[false]]
+  }
+  const patch = getVisiblePatch(field, { x: 0, y: 0 })
+  expect(patch).toBe(VisiblePatch.Flag)
+})
+
+test("should show bomb", () => {
+  const field: FieldState = {
+    bombs: [[true]],
+    flags: [[false]],
+    revealed: [[true]]
+  }
+  const patch = getVisiblePatch(field, { x: 0, y: 0 })
+  expect(patch).toBe(VisiblePatch.Bomb)
+})
+
+test("should show nearby count for empty revealed", () => {
+  const field: FieldState = {
+    bombs: [[false, true]],
+    flags: [[false, false]],
+    revealed: [[true, false]]
+  }
+  const patch = getVisiblePatch(field, { x: 0, y: 0 })
+  expect(patch).toBe(VisiblePatch.AdjacentBomb1)
+})
+
+test("should show grass for empty un-revealed", () => {
+  const field: FieldState = {
+    bombs: [[false, true]],
+    flags: [[false, false]],
+    revealed: [[false, false]]
+  }
+  const patch = getVisiblePatch(field, { x: 0, y: 0 })
+  expect(patch).toBe(VisiblePatch.Grass)
+})
+
+test("should generate a visible field", () => {
+  const field: FieldState = {
+    bombs: [
+      [true, true],
+      [false, false]
+    ],
+    flags: [
+      [true, false],
+      [false, false]
+    ],
+    revealed: [
+      [false, false],
+      [true, true]
+    ]
+  }
+
+  const visibleField = getVisibleField(field)
+  const expectedVisibleField: VisibleField = [
+    [VisiblePatch.Flag, VisiblePatch.Grass],
+    [VisiblePatch.AdjacentBomb2, VisiblePatch.AdjacentBomb2]
+  ]
+
+  expect(visibleField).toStrictEqual(expectedVisibleField)
 })
--- src/model/Field.ts
+++ src/model/Field.ts
@@ -9,23 +9,25 @@ export interface FieldSize {
 }
 
 export enum VisiblePatch {
-  Hidden,
-  Empty,
-  Bomb,
-  Flag,
-  Nearby1,
-  Nearby2,
-  Nearby3,
-  Nearby4,
-  Nearby5,
-  Nearby6,
-  Nearby7,
-  Nearby8
+  Grass = "Grass",
+  Empty = "Empty",
+  Bomb = "Bomb",
+  Flag = "Flag",
+  AdjacentBomb1 = "AdjacentBomb1",
+  AdjacentBomb2 = "AdjacentBomb2",
+  AdjacentBomb3 = "AdjacentBomb3",
+  AdjacentBomb4 = "AdjacentBomb4",
+  AdjacentBomb5 = "AdjacentBomb5",
+  AdjacentBomb6 = "AdjacentBomb6",
+  AdjacentBomb7 = "AdjacentBomb7",
+  AdjacentBomb8 = "AdjacentBomb8"
 }
+
+export type VisibleField = VisiblePatch[][]
 
 export type BoolField = boolean[][]
 
-interface FieldState {
+export interface FieldState {
   bombs: BoolField
   flags: BoolField
   revealed: BoolField
@@ -63,20 +65,71 @@ export function getAdjacentBombCount(
   }).length
 }
 
+export function getVisiblePatchForBombCount(bombCount: number): VisiblePatch {
+  return [
+    VisiblePatch.Empty,
+    VisiblePatch.AdjacentBomb1,
+    VisiblePatch.AdjacentBomb2,
+    VisiblePatch.AdjacentBomb3,
+    VisiblePatch.AdjacentBomb4,
+    VisiblePatch.AdjacentBomb5,
+    VisiblePatch.AdjacentBomb6,
+    VisiblePatch.AdjacentBomb7,
+    VisiblePatch.AdjacentBomb8
+  ][bombCount]
+}
+
+export function getVisiblePatch(
+  field: FieldState,
+  coordinate: PatchCoordinate
+): VisiblePatch {
+  const isBomb = field.bombs[coordinate.x][coordinate.y]
+  const isFlagged = field.flags[coordinate.x][coordinate.y]
+  const isRevealed = field.revealed[coordinate.x][coordinate.y]
+
+  if (isFlagged) {
+    return VisiblePatch.Flag
+  } else if (!isRevealed) {
+    return VisiblePatch.Grass
+  } else if (isBomb) {
+    return VisiblePatch.Bomb
+  } else {
+    const adjacentBombCount = getAdjacentBombCount(field.bombs, coordinate)
+    return getVisiblePatchForBombCount(adjacentBombCount)
+  }
+}
+
+function filledArray<T>(count: number, fill: T): T[] {
+  return Array(count).fill(fill, 0, count)
+}
+
+export function getVisibleField(field: FieldState): VisibleField {
+  const { width, height } = getFieldSize(field.bombs)
+  return filledArray(width, null).map((_, x) => {
+    return filledArray(height, null).map((_, y) => {
+      return getVisiblePatch(field, { x, y })
+    })
+  })
+}
+
+export function emptyField({ width, height }: FieldSize): BoolField {
+  return filledArray(width, null).map(() => {
+    return filledArray(height, false)
+  })
+}
+
 export function generateBombField(
   { width, height }: FieldSize,
   bombCount: number,
   safePatch: PatchCoordinate
 ): BoolField {
-  let field = Array(width)
-    .fill(false, 0, width)
-    .map(() => {
-      return Array(height).fill(false, 0, height)
-    })
-
   function getRandomInt(max: number): number {
     return Math.floor(Math.random() * Math.floor(max))
   }
+
+  let field = filledArray(width, null).map(() => {
+    return filledArray(height, false)
+  })
 
   let currentCount = 0
   while (currentCount < bombCount) {
